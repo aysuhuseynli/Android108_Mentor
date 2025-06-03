f42f48630727572e23e52ec9144ceb5f253b7122
aysuhuseynli <aysuhuseynli123@gmail.com>
2025-06-03 19:40:25 +0400

    main.kt

--- src/Main.kt
+++ src/Main.kt
@@ -17,88 +17,88 @@ fun main() {
     task21()
 }
 
-fun task1(){
+fun task1() {
     val name = "Aysu"
     println(name.uppercase())
     println(name.lowercase())
 }
 
-fun task2(){
+fun task2() {
     val name = "    aysu   "
     val reversedName = name.reversed()
     println(reversedName)
 }
 
-fun task3(){
+fun task3() {
     println("String-i daxil edin:")
     val input = readln()
     println("Deyismek istediyiniz herfi daxil edin:")
     val oldElement = readln()
     println("Hansi herfle deyisek?")
     val newElement = readln()
-    val newString = input.replace(oldElement,newElement)
+    val newString = input.replace(oldElement, newElement)
     println("New string: $newString")
 }
 
-fun task10(){
+fun task10() {
     val input = readln()
     val check = input[0].isDigit()
     println(check)
 }
 
-fun task11(){
+fun task11() {
     val digit = readln().toDouble()
     val result = digit.pow(3)
     Math.pow(digit, 6.0)
     println("Result: $result")
 }
 
-fun task12(){
-    val array1 = arrayOf(1,2,3,4,5)
+fun task12() {
+    val array1 = arrayOf(1, 2, 3, 4, 5)
     val sum = array1.sum()
     println("Sum: $sum")
 }
 
-fun task13(){
-    val array1 = arrayOf(4,1,6,2,8,7,3)
+fun task13() {
+    val array1 = arrayOf(4, 1, 6, 2, 8, 7, 3)
     val sortedArray = array1.sortedArray()
     println(sortedArray.contentToString())
 }
 
-fun task14(){
+fun task14() {
     val name = "   aysu   "
     println(name.trim())
 }
 
-fun task15(){
-    val array1 = arrayOf(1,2,3,4,5,6)
+fun task15() {
+    val array1 = arrayOf(1, 2, 3, 4, 5, 6)
     println(array1.contains(7))
 }
 
-fun task16(){
+fun task16() {
     val name = "Aysu Huseynli"
-    val newString = name.substring(2,6)
+    val newString = name.substring(2, 6)
     println(newString)
 }
 
-fun task19(){
-    val array1 = arrayOf(1,2,3,7,9,8)
-    val check = array1.filter { it%2 == 0  }
+fun task19() {
+    val array1 = arrayOf(1, 2, 3, 7, 9, 8)
+    val check = array1.filter { it % 2 == 0 }
     println(check)
 }
 
-fun task18(){
+fun task18() {
     val name = "aysu huseynli"
     val vowels = name.filter { it in "aeouəüöiı" }
     println(vowels.length)
 }
 
-fun task20(){
-    val array1 = arrayOf(1,9,3,6,2)
+fun task20() {
+    val array1 = arrayOf(1, 9, 3, 6, 2)
     println(array1.max())
 }
 
-fun task21(){
+fun task21() {
     val eded = 3.7
     println(eded.roundToInt())
 }
